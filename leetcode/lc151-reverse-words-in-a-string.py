2905698c4d3ba49f55e1af4aee2cb77e85a4b0b8
Mauricio Mutte <mauriciomutte.mm@gmail.com>
2023-06-16 22:23:21 -0300

    chore(leetcode): change 151 solution to use two pointers

--- leetcode/lc151-reverse-words-in-a-string.py
+++ leetcode/lc151-reverse-words-in-a-string.py
@@ -5,7 +5,28 @@ import unittest
 
 class Solution:
     def reverseWords(self, s: str) -> str:
-        return " ".join(reversed(s.split()))
+        words = []
+        temp_word = ""
+        for c in s:
+            if c != " ":
+                temp_word += c
+            elif temp_word != "":
+                words.append(temp_word)
+                temp_word = ""
+
+        if temp_word != "":
+            words.append(temp_word)
+
+        i = 0
+        j = len(words) - 1
+        while i < j:
+            temp = words[i]
+            words[i] = words[j]
+            words[j] = temp
+            i += 1
+            j -= 1
+
+        return " ".join(words)
 
 
 class TestSolution(unittest.TestCase):
